bf18a63e40ffa4e799f02e19f9a171bcd46fa6f7
Rose Wisotzky <rose@rosewisotzky.com>
2019-03-29 11:43:05 -0500

    Created normalized database in json

---
+++ src/scripts/main.js
@@ -0,0 +1,16 @@
+console.log("ride the wave buddy surfs up")
+// Once your data is normalized, use your DOM skills to display a card for each employee. It should display the employee name, the name of their department, and which computer they are using
+
+// <article class="employee">
+//     <header class="employee__name">
+//         <h1>Rainu Ittycheriah</h1>
+//     </header>
+//     <section class="employee__department">
+//         Works in the IT department
+//     </section>
+//     <section class="employee__computer">
+//         Currently using a 2015 MacBook Pro
+//     </section>
+// </article>
+
+const displayContainer = document.querySelector(displayContainer)
